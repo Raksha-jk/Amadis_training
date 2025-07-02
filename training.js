75bc5634a7506ee0c7485784a1ba5a716a236d9c
Raksha-jk <rakshajk1904@gmail.com>
2025-07-02 15:49:00 +0530

    initial commit

--- training.js
+++ training.js
@@ -3,7 +3,7 @@ const num2 = 3;
 
 // add two numbers
 const sum = num1 + num2;
-const sub = num1 - num2;
+
 // display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
 console.log('The sub of ' + num1 - ' and ' + num2 + ' is: ' + sub);
